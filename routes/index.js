0243819f6d6400cce11c109575e86179fbb80649
Spencer Jackson <spencerja8@gmail.com>
2017-02-02 19:31:23 -0500

    second commit

--- routes/index.js
+++ routes/index.js
@@ -9,25 +9,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/mom', function(req, res, next) {
-  res.render('index', { title: 'Mom',
+  res.render('mom', { title: 'Mom',
                         info: 'My mom lives in Bracebridge and works at my step dads business'
   });
 });
 
 router.get('/dad', function(req, res, next) {
-  res.render('index', { title: 'Dad',
+  res.render('dad', { title: 'Dad',
                         info: 'My dad is retired and lives in Innisfil'
   });
 });
 
-router.get('/Zach', function(req, res, next) {
-  res.render('index', { title: 'Zach',
+router.get('/zach', function(req, res, next) {
+  res.render('zach', { title: 'Zach',
                         info: 'Older brother. Runs a succesful meme page on facebook'
   });
 });
 
 router.get('/forrester', function(req, res, next) {
-  res.render('index', { title: 'Forrester',
+  res.render('forrester', { title: 'Forrester',
                         info: 'Younger brother. Works as a cook at a pub'
   });
 });
